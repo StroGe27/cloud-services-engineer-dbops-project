2464f44796eceb4096d9439a407adc771dfe2ba3
Stroganov Georgiy <stroganovgeorgi01@gmail.com>
2026-03-01 09:25:09 +0000

    add commit to run CI

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -8,11 +8,11 @@ ADD CONSTRAINT product_pkey PRIMARY KEY (id);
 ALTER TABLE orders
 ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
 
-ALTER TABLE order_product 
-ADD CONSTRAINT fk_order_product_product 
+ALTER TABLE order_product
+ADD CONSTRAINT fk_order_product_product
 FOREIGN KEY (product_id) REFERENCES product(id);
-ALTER TABLE order_product 
-ADD CONSTRAINT fk_order_product_orders 
+ALTER TABLE order_product
+ADD CONSTRAINT fk_order_product_orders
 FOREIGN KEY (order_id) REFERENCES orders(id);
 
 DROP TABLE product_info;
